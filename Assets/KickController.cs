775ae6d705c3def5549b720f10c7278c74cf6452
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2016-10-30 22:03:38 -0400

    ik scripts

---
+++ Assets/KickController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))] 
+
+public class KickController : MonoBehaviour {
+    
+    protected Animator animator;
+    
+    private bool ikActive = false;
+    private Transform rightHandObj = null;
+    private Transform lookObj = null;
+
+    void Start () 
+    {
+        animator = GetComponent<Animator>();
+    }
+
+	void kick(Transform rightHandObj, Transform lookObj) {
+		this.rightHandObj = rightHandObj;
+		this.lookObj = lookObj;
+		ikActive = true;
+	}
+
+	void unkick() {
+		ikActive = false;
+		rightHandObj = null;
+		lookObj = null;
+	}
+    
+    //a callback for calculating IK
+    void OnAnimatorIK()
+    {
+        if(animator) {
+            
+            //if the IK is active, set the position and rotation directly to the goal. 
+            if(ikActive) {
+
+                // Set the look target position, if one has been assigned
+                if(lookObj != null) {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(lookObj.position);
+                }    
+
+                // Set the right hand target position and rotation, if one has been assigned
+                if(rightHandObj != null) {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightFoot,1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightFoot,1);  
+                    animator.SetIKPosition(AvatarIKGoal.RightFoot,rightHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightFoot,rightHandObj.rotation);
+                }        
+                
+            }
+            
+            //if the IK is not active, set the position and rotation of the hand and head back to the original position
+            else {          
+                animator.SetIKPositionWeight(AvatarIKGoal.RightFoot,0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightFoot,0); 
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }    
+}
+
---
+++ Assets/PunchController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))] 
+
+public class Punch : MonoBehaviour {
+    
+    protected Animator animator;
+    
+    private bool active = false;
+    private Transform rightHandObj = null;
+    private Transform lookObj = null;
+
+    void Start () 
+    {
+        animator = GetComponent<Animator>();
+    }
+
+	void punch(Transform rightHandObj, Transform lookObj) {
+		this.rightHandObj = rightHandObj;
+		this.lookObj = lookObj;
+		ikActive = true;
+	}
+
+	void unpunch() {
+		ikActive = false;
+		rightHandObj = null;
+		lookObj = null;
+	}
+    
+    //a callback for calculating IK
+    void OnAnimatorIK()
+    {
+        if(animator) {
+            
+            //if the IK is active, set the position and rotation directly to the goal. 
+            if(ikActive) {
+
+                // Set the look target position, if one has been assigned
+                if(lookObj != null) {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(lookObj.position);
+                }    
+
+                // Set the right hand target position and rotation, if one has been assigned
+                if(rightHandObj != null) {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand,1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand,1);  
+                    animator.SetIKPosition(AvatarIKGoal.RightHand,rightHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand,rightHandObj.rotation);
+                }        
+                
+            }
+            
+            //if the IK is not active, set the position and rotation of the hand and head back to the original position
+            else {          
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand,0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand,0); 
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }    
+}
+
+
